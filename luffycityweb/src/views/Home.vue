d84826ce33572b56b39acaa89f2c03d2d900f4ec
org_ubuntu <791359406@qq.com>
2025-05-25 06:50:25 +0800

    feature:axios配置和整合

--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -14,6 +14,21 @@ import Header from  '../components/Header.vue'
 import Footer from  '../components/Footer.vue'
 
 
+//测试cors的跨域配置是否有问题
+// import axios  from "axios";
+// const http=axios.create()
+//
+// //测试服务端的跨域是否配置成功 get post put
+// http.get('http://api.luffycity.cn:8000/home/test').then(res=>{
+//   console.log(res.data)
+// })
+
+import http from '../utils/http.js'
+http.get('/home/test').then(res=>{
+  console.log(res.data)
+})
+
+
     const store=reactive({
       value2:null,
       colors:['#99A98F','#F7BA2A','#FF9900'],
