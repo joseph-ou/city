18e0c8a7ec9faa43f744b33e6fba56c99dfa6f55
org_ubuntu <791359406@qq.com>
2025-06-09 10:24:45 +0800

    feature:前端注册页面初步配置

--- luffycityweb/src/views/Register.vue
+++ luffycityweb/src/views/Register.vue
@@ -35,8 +35,13 @@
 
 
 <script setup>
-import {reactive} from "vue";
 
+import {reactive, defineEmits} from "vue"
+import { ElMessage } from 'element-plus'
+import {useStore} from "vuex"
+import "../utils/TCaptcha"
+
+const store=useStore()
 
 const state=reactive({
   mobile:'',
@@ -50,5 +55,156 @@ const state=reactive({
 
 
 <style scoped>
+.box{
+	width: 100%;
+  height: 100%;
+	position: relative;
+  overflow: hidden;
+}
+.box img{
+	width: 100%;
+  min-height: 100%;
+}
+.box .login {
+	position: absolute;
+	width: 500px;
+	height: 400px;
+	left: 0;
+  margin: auto;
+  right: 0;
+  bottom: 0;
+  top: -438px;
+}
 
+.login-title{
+     width: 100%;
+    text-align: center;
+}
+.login-title img{
+    width: 190px;
+    height: auto;
+}
+.login-title p{
+    font-size: 18px;
+    color: #fff;
+    letter-spacing: .29px;
+    padding-top: 10px;
+    padding-bottom: 50px;
+}
+.login_box{
+    width: 400px;
+    height: auto;
+    background: #fff;
+    box-shadow: 0 2px 4px 0 rgba(0,0,0,.5);
+    border-radius: 4px;
+    margin: 0 auto;
+    padding-bottom: 40px;
+    padding-top: 50px;
+}
+.title{
+	font-size: 20px;
+	color: #9b9b9b;
+	letter-spacing: .32px;
+	border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  padding: 0px 60px 0 60px;
+  margin-bottom: 20px;
+  cursor: pointer;
+}
+.title span.active{
+	color: #4a4a4a;
+}
+
+.inp{
+	width: 350px;
+	margin: 0 auto;
+}
+.inp .code{
+  width: 190px;
+  margin-right: 16px;
+}
+#get_code{
+ margin-top: 6px;
+}
+.inp input{
+    outline: 0;
+    width: 100%;
+    height: 45px;
+    border-radius: 4px;
+    border: 1px solid #d9d9d9;
+    text-indent: 20px;
+    font-size: 14px;
+    background: #fff !important;
+}
+.inp input.user{
+    margin-bottom: 16px;
+}
+.inp .rember{
+     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    margin-top: 10px;
+}
+.inp .rember p:first-of-type{
+    font-size: 12px;
+    color: #4a4a4a;
+    letter-spacing: .19px;
+    margin-left: 22px;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+    /*position: relative;*/
+}
+.inp .rember p:nth-of-type(2){
+    font-size: 14px;
+    color: #9b9b9b;
+    letter-spacing: .19px;
+    cursor: pointer;
+}
+
+.inp .rember input{
+    outline: 0;
+    width: 30px;
+    height: 45px;
+    border-radius: 4px;
+    border: 1px solid #d9d9d9;
+    text-indent: 20px;
+    font-size: 14px;
+    background: #fff !important;
+    vertical-align: middle;
+    margin-right: 4px;
+}
+
+.inp .rember p span{
+    display: inline-block;
+  font-size: 12px;
+  width: 100px;
+}
+.login_btn{
+    cursor: pointer;
+    width: 100%;
+    height: 45px;
+    background: #84cc39;
+    border-radius: 5px;
+    font-size: 16px;
+    color: #fff;
+    letter-spacing: .26px;
+    margin-top: 30px;
+    border: none;
+    outline: none;
+}
+.inp .go_login{
+    text-align: center;
+    font-size: 14px;
+    color: #9b9b9b;
+    letter-spacing: .26px;
+    padding-top: 20px;
+}
+.inp .go_login span{
+    color: #84cc39;
+    cursor: pointer;
+}
 </style>
