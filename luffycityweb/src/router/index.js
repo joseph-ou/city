e064862a5d8371407c7a883959a722b2ca3e7eb8
org_ubuntu <791359406@qq.com>
2025-07-16 04:46:30 +0800

    feature:展示列表页面

--- luffycityweb/src/router/index.js
+++ luffycityweb/src/router/index.js
@@ -43,6 +43,17 @@ const routes=[
     name: "User",
     component: ()=> import("../views/User.vue"),
   },
+
+    {
+    meta:{
+        title: "luffy2.0-课程列表",
+        keepAlive: true,
+        authorization:true,
+    },
+    path: '/project',
+    name: "Course",
+    component: ()=> import("../views/Course.vue"),
+  },
 ]
 
 //路由对象实例化
